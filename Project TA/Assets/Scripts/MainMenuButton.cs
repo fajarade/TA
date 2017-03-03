26757c1508224f9c0a3d73999cc8b0ef6151dd1f
fajarade <fajar_ade@hotmail.com>
2017-03-03 14:44:41 +0700

    connect to steering wheel

---
+++ Project TA/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButton : MonoBehaviour {
+
+	public void Button1(){
+		Application.LoadLevel("Ujian1");
+	}
+
+	public void Button2(){
+		Application.LoadLevel("Ujian2");
+	}
+
+	public void Button3(){
+		Application.LoadLevel("Ujian3");
+	}
+
+	public void Button4(){
+		Application.LoadLevel("Ujian4");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
